a7c0a83d12d3d5c216b3de26857bc0e587266cfa
Kenneth Dombrowski <kdombrowski@gmail.com>
2026-06-05 15:30:31 -0400

    fix test_fetch_iso3166_countries(); update rate tables to fix broken usps tests; TODO: FCPIS has introduced a new weight class (8-32oz split into 8-16oz & 16-32oz)

--- discoship/tests/countries/test_fetch.py
+++ discoship/tests/countries/test_fetch.py
@@ -5,11 +5,14 @@ from discoship.countries import fetch
 from discoship.testing import load_bs4_data_fixture, load_saved_output
 
 
-def test_fetch_iso3166_countries():
+def test_fetch_iso3166_countries(mocker):
+    # fetch_url-fetch_iso3166_countries.htm created by @save_output_for_caller
     filename = 'fetch_url-fetch_iso3166_countries.htm'
-    # html = load_saved_output(filename)
-    # mocker.patch('discoship.countries.fetch_usps_rate_tables', return_value=html)
+    html = load_saved_output(filename)
+    mock_fetch_url = mocker.patch('discoship.countries.fetch.fetch_url', return_value=html)
+    # countries_data is list of tuples: (country_name, sovereignty, code2, code3)
     countries_data = fetch.fetch_iso3166_countries()
-    print(countries_data)
-    fetch.fetch_iso3166_countries.assert_called()
+    mock_fetch_url.assert_called()
+    expect_usa = ('United States', 'UN member', 'US', 'USA')
+    assert expect_usa in countries_data
 
